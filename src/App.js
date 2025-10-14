40f833cbc2c66359bccc89c8358ba9ac973f6eef
Glender Nieto <glennder2020@gmail.com>
2025-10-14 15:19:00 +0000

    Correcion al componente Game

--- src/App.js
+++ src/App.js
@@ -8,23 +8,19 @@ function Scuare({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xInNext, setXInNext] = useState(true);
-  const [squares, setScuares] = useState(Array(9).fill(null));
-
+function Board({ xInNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
-    const nextSquare = squares.slice();
+    const nextSquares = squares.slice();
     if (xInNext) {
-      nextSquare[i] = "X";
+      nextSquares[i] = "X";
     } else {
-      nextSquare[i] = "O";
+      nextSquares[i] = "O";
     }
 
-    setScuares(nextSquare);
-    setXInNext(!xInNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -57,7 +53,29 @@ export default function Board() {
   );
 }
 
-function calculateWinner(square) {
+export default function Game() {
+  const [xInNext, setXInNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXInNext(!xInNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xInNext={xInNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/**TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -71,8 +89,8 @@ function calculateWinner(square) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (square[a] && square[a] === square[b] && square[a] === square[c]) {
-      return square[a];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
   return null;
